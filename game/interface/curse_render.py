0859b4f55547832207852d17db7494456b5e005e
Divyansh-Test <nautiyaldivyansh98@gmail.com>
2026-05-19 20:20:24 +0000

    Successfully implemented the menu with arrow buttons and is working
    Next task is to make the time of both diff and also the render the map.

--- game/interface/curse_render.py
+++ game/interface/curse_render.py
@@ -1,55 +1,78 @@
 import curses
 import time
-from inputs import get_input  
+from inputs import get_input
 from render import render
 
 
-menu_items=["Description","Follow","Inventory","Skills","Quit"]
+menu_items = ["Description", "Follow", "Inventory", "Skills", "Quit"]
 
 
-#menu_items=["New Game","Load Game","Options","Quit"]
+# menu_items=["New Game","Load Game","Options","Quit"]
 
 
 def Map_win(win):
-  win.clear()
-  win.border()
-  win.addstr(1,1,"Map")
-  win.refresh()
-def Main_menu(win,):
-  pass
-
-def Char_menu(win,key):
-  win.clear()
-  win.border()
-  win.addstr(1,1,"Char")
-  win.refresh()
+    win.clear()
+    win.border()
+    win.addstr(1, 1, "Map")
+    win.refresh()
 
 
+def Main_menu(
+    win,
+):
+    pass
 
 
+def Char_menu(stdscr, win, selected):
+    win.clear()
+    win.border()
+    
+
+    for i in range(len(menu_items)):
+        if selected == i:
+            stdscr.attron(curses.color_pair(1))
+            
+            win.addstr(i + 1, 1, menu_items[i],curses.color_pair(1))
+            stdscr.attroff(curses.color_pair(1))
+            
+        else:
+            win.addstr(i + 1, 1, menu_items[i])
+
+    
+
+    win.refresh()
 
 
 def main(stdscr):
-  curses.curs_set(0)
-  h,w=stdscr.getmaxyx()
-  map_win=curses.newwin(10,10,0,0)
-  char_win=curses.newwin(10,10,0,10)
-  #menu_win=curses.newwin(10,10,0,10)
-  stdscr.nodelay(1)
-  while True:
-    key=stdscr.getch()
-    get_input(key)
+    curses.curs_set(0)
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_WHITE, curses.COLOR_RED)
+    h, w = stdscr.getmaxyx()
+    map_win = curses.newwin(10, 10, 0, 0)
+    char_win = curses.newwin(10, 10, 0, 10)
+    curses.napms(1000)
+    # menu_win=curses.newwin(10,10,0,10)
+    stdscr.nodelay(1)
+    selected =0
+    while True:
+        key = stdscr.getch()
+        key=get_input(key)
+        
+        
+        if key == "down":
+            selected += 1
+        elif key == "up":
+            selected -= 1
 
-    Map_win(map_win)
-    Char_menu(char_win,key)
-    stdscr.getch()
+        map_win.addstr(1, 1,key)
 
-  time.delay(0.5)
+        selected = selected % len(menu_items)
+
+        map_win.refresh()
+        Char_menu(stdscr, char_win,selected)
+        stdscr.getch()
+        time.sleep(1)
 
 
-if __name__=="__main__":
-   curses.wrapper(main)
-
-  
-  
-  
+if __name__ == "__main__":
+    curses.wrapper(main)
